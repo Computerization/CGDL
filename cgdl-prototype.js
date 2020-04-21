d1df31cf9a9662cf40b666ba3575428c540e441b
ETwilight <hellobrian18@163.com>
2020-04-21 18:47:10 +0800

    The basic texture module is included in "basic_texture_module.html".
    If there are any useless or inefficient code, please help optimize them, thanks.
    The document "basic_texture_module.html" supports both Chrome and Edge browser.

--- cgdl-prototype.js
+++ cgdl-prototype.js
@@ -5,17 +5,17 @@
 var timer = null;
 
 class CGDL {
-
+    /*
 	#canvas;
 	#context;
-
+    */
 	constructor() {
-		this.#canvas=document.getElementById("cgdl-canvas");
-		if(this.#canvas.getContext){
-			this.#context=this.#canvas.getContext("2d");
+		this.canvas=document.getElementById("cgdl-canvas");
+		if(this.canvas.getContext){
+			this.context=this.canvas.getContext("2d");
 		}
 	}
-
+    /*
 	get canvas() {
 		return this.#canvas;
 	}
@@ -23,20 +23,21 @@ class CGDL {
 	get context() {
 		return this.#context;
 	}
+	*/
 }
 
 class CGDLSystem {
-
+    /*
 	#id;
 	#type;
 	#context;
-
+    */
 	constructor(id, type, context) {
-		this.#id = id;
-		this.#type = type;
-		this.#context = context;
+		this.id = id;
+		this.type = type;
+		this.context = context;
 	}
-
+    /*
 	get id() {
 		return this.#id;
 	}
@@ -48,53 +49,46 @@ class CGDLSystem {
 	get context() {
 		return this.#context;
 	}
+	*/
 }
 
 class Texture extends CGDLSystem {
-	
+	/*
 	#src;
 	#img;
 	#height;
 	#width;
-	
+	*/
 	constructor(id, context, src) {
 		super(id, 0, context);
-		this.#src = src;
+		this.src = src;
 	}
 
-	set height(h) {
-		this.#height = h;
+	setHeight(h) {
+		this.height = h;
 	}
 
-	set width(w) {
-		this.#width = w;
-	}
-
-	static checkImgLoad(height, width) {
-		//console.log("a");
-		var dom = document.getElementById("new_img");
-		dom.onload = function() {
-			console.log("load");
-			var context = document.getElementById("cgdl-canvas").getContext("2d");
-			context.drawImage(dom2, height, width);
-			console.log(height);
-		}
+	setWidth(w) {
+		this.width = w;
 	}
 
 	show() {
 		var body = document.getElementById("bd");
-		body.innerHTML = body.innerHTML + "<div height='0' width='0'>" + "<img src='" + this.#src + "' id='new_img' height='0' width='0'/></div>";
-		//Texture.checkImgLoad(this.#height,this.#width);
-		var dom = document.getElementById("new_img");
-		var height = this.#height;
-		var width = this.#width;
+		body.innerHTML = body.innerHTML + "<div height='0' width='0'>" + "<img src='" + this.src + "' id='new_img' height='50' width='50'/></div>";
+		var img = document.getElementById("new_img");
+		console.log(img);
+		var height = this.height;
+		var width = this.width;
 		var context = this.context;
+		context.drawImage(img, height, width);
+		/*
 		dom.onload = function() {
 			console.log("onload");
 			console.log(dom);
 			context.drawImage(dom, height, width);
 			console.log("Drawn");
 		}
+		*/
 	}
 
 }
@@ -108,25 +102,27 @@ class Geometric extends CGDLSystem {
 }
 
 class Actor extends CGDLSystem {
-
+    /*
 	static APPEARANCE_TEXTURE = 0;
 	static APPEARANCE_GEOMETRIC = 1;
 	#appearanceType = undefined;
-
+    */
 	constructor(id, context, appearanceType) {
 		super(id, 2, context);
-		this.#appearanceType = appearanceType;
+		this.appearanceType = appearanceType;
+		this.APPEARANCE_TEXTURE = 0;
+		this.APPEARANCE_GEOMETRIC = 1;
 	}
 
 	bindTexture(textureData) {
-		if (this.#appearanceType != Actor.APPEARANCE_TEXTURE) {
+		if (this.appearanceType != this.APPEARANCE_TEXTURE) {
 			throw "CGDLError : Apperance Type does not match with this method.";
 		}
 
 	}
 	
 	bindGeometric(geometricData) {
-		if (this.#appearanceType != Actor.APPEARANCE_GEOMETRIC) {
+		if (this.appearanceType != this.APPEARANCE_GEOMETRIC) {
 			throw "CGDLError : Apperance Type does not match with this method.";
 		}
 	}
